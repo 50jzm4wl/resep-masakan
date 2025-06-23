e904e4d45c83e0eab5ac54d7b964686ca7fb6ada
50jzm4wl <novera.cinthania24@students.utdi.ac.id>
2025-06-24 00:34:23 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,27 @@
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open("res-cache").then(c => c.addAll([
-    "/index.html", "/style.css", "/app.js", "/manifest.json", "/assets/icon.png"
-  ])));
+  e.waitUntil(
+    caches.open("res-cache").then(cache => {
+      return cache.addAll([
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/manifest.json",
+        "/assets/icon.png"
+        "/assets/nasi-goreng.jpg",
+        "/assets/sate-ayam.jpg",
+        "/assets/pancake.jpg",
+        "/assets/rendang.jpg",
+        "/assets/gado-gado.jpg",
+
+      ]);
+    })
+  );
 });
+
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
